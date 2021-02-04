c900ec19a7edab546c7bbbb163e088d556586a6d
Jannik Baranczyk <jb@connect2.de>
2021-02-04 14:26:47 +0100

    disable content editor

--- inc/admin.php
+++ inc/admin.php
@@ -2,6 +2,10 @@
   function remove_page_attribute_meta_box() {
     remove_meta_box('pageparentdiv', 'page', 'normal');
   }
-
   add_action('admin_menu', 'remove_page_attribute_meta_box');
+
+  function remove_editor_from_post_type() {
+    remove_post_type_support( 'page', 'editor' );
+  }
+  add_action('init', 'remove_editor_from_post_type');
 ?>
